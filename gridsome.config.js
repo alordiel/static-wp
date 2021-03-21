d77cb6fbd5023b5ade73d11dd1985884ba495220
alexander vasilev <alexander.vasilev@protonmail.com>
2021-03-21 17:01:55 +0200

    initial

---
+++ gridsome.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteName: 'Gridsome',
+  siteDescription: 'A WordPress starter for Gridsome',
+
+  templates: {
+    WordPressCategory: '/category/:slug', // adds a route for the "category" post type (Optional)
+    WordPressPost: '/:year/:month/:day/:slug', // adds a route for the "post" post type (Optional)
+    WordPressPostTag: '/tag/:slug' // adds a route for the "post_tag" post type (Optional)
+  },
+
+  plugins: [
+    {
+      use: '@gridsome/source-wordpress',
+      options: {
+        baseUrl: 'https://arsofia.com', // required
+        typeName: 'WordPress', // GraphQL schema name (Optional)
+        apiBase: 'wp-json',
+        perPage: 50,
+        concurrent: 10
+      }
+    }
+  ]
+}
